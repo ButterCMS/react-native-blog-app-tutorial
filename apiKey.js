3628903102f5e26f2ad426dfe14e5c0f2eaf7f02
leviOnNet <65132823+leviOnNet@users.noreply.github.com>
2022-11-18 10:34:22 +0200

    Update apiKey.js

--- apiKey.js
+++ apiKey.js
@@ -1,5 +1,5 @@
 import Butter from 'buttercms'
 
-const butter = Butter('c3c37caed6eaa3141e464fc82cab8f83cae7925f')
+const butter = Butter('<enter your ButterCMS API key here>')
 
 export default butter
